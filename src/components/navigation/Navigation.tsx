de1fbf268cb73e2b78614bedb581764217b53ef2
JI20 <jonas.itt@tum.de>
2024-11-23 23:36:23 +0100

    fix navbar positioning on mobile

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Navigation = () => {
   return (
     <div
-      className="absolute z-2000 bottom-0 left-0 h-[50px] 
+      className="fixed z-2000 bottom-0 left-0 h-[50px] 
     w-screen bg-white flex justify-between items-center px-32"
     >
       <Link to="/">Map</Link>
