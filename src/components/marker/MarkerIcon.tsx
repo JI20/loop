c999c684b6404d08b83377b415261ca04f788b23
JI20 <jonas.itt@tum.de>
2024-11-23 12:34:12 +0100

    Work further on marker icon

--- src/components/marker/MarkerIcon.tsx
+++ src/components/marker/MarkerIcon.tsx
@@ -7,14 +7,33 @@ export const MarkerIcon = () => {
       <div
         className={`${
           openDetail
-            ? "w-[90px] h-[40px] rounded-4"
+            ? "w-[250px] h-[120px] rounded-[18px]"
             : "w-[40px] h-[40px] rounded-full"
-        } bg-white border-black border-[2px]`}
+        } bg-white border-black transition-all duration-75`}
         onClick={() => {
           setOpenDetail((state) => !state);
         }}
       >
-        {openDetail && <h1>I'm open!</h1>}
+        {openDetail && (
+          <div className="h-full w-full px-4 py-3 flex flex-col justify-between">
+            <div>
+              <h1 className="font-['Poppins'] font-bold text-xl">Basketball</h1>
+              <p className="text-l">
+                <span className="text-orange">@Jonas</span> wants to play
+                Basketball!
+              </p>
+            </div>
+            <div>
+              <button className="bg-blue px-7 py-1 rounded-full text-white">
+                Join!
+              </button>
+              <div className="flex justify-between w-full">
+                <p>3/5</p>
+                <p>in 30 min</p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
