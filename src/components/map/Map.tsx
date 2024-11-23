772df02686fbc48cf0165e6ead3f63e53ee98ba7
JI20 <jonas.itt@tum.de>
2024-11-23 21:28:38 +0100

    Fix unused imports

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -4,7 +4,6 @@ import { Marker } from "@adamscybot/react-leaflet-component-marker";
 import { MarkerIcon } from "../marker/MarkerIcon";
 import { UserEvent } from "../../config/events";
 import { useState } from "react";
-import { categories } from "../../config/categories";
 import { categoriesColors } from "../../config/categories";
 
 const position: LatLngTuple = [48.135, 11.582];
