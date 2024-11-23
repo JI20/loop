509cf3510e1066bd557e8a4875659c7a6208815d
JI20 <jonas.itt@tum.de>
2024-11-23 08:41:42 +0100

    Change title page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1 className="font-bold font-['Poppins'] text-[8rem]">Loop.</h1>
       <p className="text-l">
-        Stay in the loop and connect to others! Let's fight the lonliness - together.
+        Stay in the loop and connect to others! Let's fight the lonliness -
+        together.
       </p>
+      <p className="text-l">Our app is still in the works!</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
