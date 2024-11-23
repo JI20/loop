66ac2a9d8b5b5184e31043d47f72719f342c817f
JI20 <jonas.itt@tum.de>
2024-11-23 08:05:32 +0100

    Modify tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code className='text-3xl bg-black'>src/App.tsx</code> and save to test HMR
+          Edit <code className='text-3xl bg-purple'>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
