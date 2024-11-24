7b8e72f619dc2e531e2fd9728eaa60d0c5a4028d
JI20 <jonas.itt@tum.de>
2024-11-24 09:34:43 +0100

    Add filters

--- src/components/exploreCards/exploreCard.tsx
+++ src/components/exploreCards/exploreCard.tsx
@@ -9,21 +9,30 @@ export const ExploreCard = (props: any) => {
     <div
       className={`${
         !(event.participants === 0) ? "border-orange" : "border-black"
-      } border-[2px] w-[150px] h-[220px] sm:w-[160px] sm:h-[200px] md:w-[200px] md:h-[200px] lg:w-[250px] lg:h-[250px] relative flex flex-col justify-between`}
+      } border-[2px] w-[160px] h-[250px] sm:w-[160px] sm:h-[200px] md:w-[200px] md:h-[200px] lg:w-[250px] lg:h-[250px] relative flex flex-col justify-between`}
     >
       <div className="bg-white h-[100px]">
-        <h1 className="text-l font-bold">{event.name}</h1>
+        <h1 className="mx-1 text-l font-bold">{event.name}</h1>
         {!(event.participants === 0) && (
-          <p className="text-orange">@{event.host}</p>
+          <>
+            <p className="mx-1 text-orange">@{event.host}</p>
+          </>
         )}
       </div>
       {!(event.participants === 0) && (
         <div className="flex justify-between">
-          <p>
+          <p className="text-white">
             {event.participants}/{event.max_number_of_participants}
           </p>
-          <p>{`${moment(date).format("HH:mm")}`}</p>
+          <p className="text-white">{`${moment(date).format("HH:mm")}`}</p>
         </div>
+      )}
+      {event.participants === 0 && (
+        <>
+          <button className="mx-1 mb-2 bg-blue px-2 text-white py-1">
+            Start
+          </button>
+        </>
       )}
       <img
         className="absolute object-cover z-[-1] w-full h-full"
