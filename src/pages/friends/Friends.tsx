7cfad7a5e0807861e6a6ad9f39f41bc99c765d17
mryammar <mariamammar023@gmail.com>
2024-11-24 03:14:49 +0100

    fixed error

--- src/pages/friends/Friends.tsx
+++ src/pages/friends/Friends.tsx
@@ -4,11 +4,11 @@ function Friends() {
   return (
     <>
       <div className="grid grid-cols-1 divide-y">
-        <Friendlistentry name="Leon" age="19" />
-        <Friendlistentry name="Ola" age="24" />
-        <Friendlistentry name="Helen" age="18" />
-        <Friendlistentry name="Leon" age="20" />
-        <Friendlistentry name="Talia" age="21" />
+        <Friendlistentry name="Leon" age={19} />
+        <Friendlistentry name="Ola" age={24} />
+        <Friendlistentry name="Helen" age={18} />
+        <Friendlistentry name="Leon" age={20} />
+        <Friendlistentry name="Talia" age={21} />
       </div>
     </>
   );
